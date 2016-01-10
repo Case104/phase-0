01c0506eee7a3d70d8040c089217878537e40a3f
Case104 <jpcase104@gmail.com>
2016-01-10 17:48:51 -0500

    Solo challenges, and both pairing assignments updated.

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,53 @@
+# Conditions:
+	# Group size must be between 3 - 5
+	# prefer 4 - 5
+
+# INPUT:
+	# list of names
+
+# OUTPUT: 
+	# groups of people
+
+# METHOD: 
+	# take list of names, SPLIT by , set equal to array.
+	# IF array.length <= 5, return list.join
+	# ELSE randomize array
+		# IF array.length divisible by 5 OR remainder 4 OR remainder 3, SLICE array by 5
+		# ELSIF array.length divisible by 4 OR remainder 3, SLICE by 4
+		# ELSIF array.length divisibly by 3 SLICE by 3
+
+		# CURRENT METHOD WILL HAVE "HOLES" off the top of my head, 17 and 22 will break it.
+
+def form_groups(list)
+	list_array = list.split(", ")
+	list_length = list_array.length
+	if list_length <= 5
+		return list_array
+	else
+		list_array.shuffle!
+		if list_length % 5 == 0 || list_length % 5 == 4 || list_length % 5 == 3
+			return list_array.each_slice(5).to_a
+		elsif list_length % 4 == 0 || list_length % 4 == 3
+			return list_array.each_slice(4).to_a
+		elsif list_length % 3 == 0
+			return list_array.each_slice(3).to_a
+		end
+	end
+end
+
+=begin
+REFACTOR:
+
+I'm way behind this week so I don't have enough time to refactor all of my solutions. That being said, I would probably use a switch statement (case) for my if and elsif conditions. I have no "else" so case would most likely work better. I would also try to think of a way to solve for numbers like 17 and 22, which do not meet any of the current criteria. I believe I can keep the same line of thought and figure out a solution for thise numbers.
+
+What was the most interesting and most difficult part of this challenge?
+	I thought this one was fairly simple. Its interesting to think about what would break it. I feel like I want to write a program designed to try to break it.
+Do you feel you are improving in your ability to write pseudocode and break the problem down?
+	This was my most successful psuedocode yet. I think as I add more methods to my vocabulary, psuedo code ill get easier.
+Was your approach for automating this task a good solution? What could have made it even better?
+	Ditto what I said in refactor. DRY
+What data structure did you decide to store the accountability groups in and why?
+	I decided on an array. I didn't have two or more corresponding peices of data to store, so I felt that an array was warranted.
+What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+	I learned to plan my time better. I had a busy work week and didn't even look at the workload until Thursday. I should have made DBC the priority.
+end
--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -1,6 +1,6 @@
 # Pad an Array
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge with Jonathan
 
 # I spent [] hours on this challenge.
 
@@ -14,22 +14,84 @@
 # 0. Pseudocode
 
 # What is the input?
+# The input is going to be an array, an integer for the minimum array size, and an optional value for the "padding"
+
 # What is the output? (i.e. What should the code return?)
+# If the minimum size is less than or equal to the size of the array, the output will be the original array. If the minimum size is greater than the size of the array, the output will be the original array plus the optional value x number of times.
+
 # What are the steps needed to solve the problem?
+# 1. Calculate the difference between the minimum size and the array length
+# 2. Create an if/else statement
+#   - IF the difference is <= 0, then returns array
+#   - ELSE push the value difference times
+#   - return the array
 
 
 # 1. Initial Solution
+=begin
 def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+  difference = min_size - array.length
+  if difference <= 0
+    return array
+  else
+    difference.times{ array.push(value) }
+    return array
+  end
 end
 
 def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+  newarray = array.clone
+  difference = min_size - newarray.length
+  if difference <= 0
+    return newarray
+  else
+    difference.times{ newarray.push(value) }
+    return newarray
+  end
 end
-
+=end
 
 # 3. Refactored Solution
 
+def pad!(array, min_size, value = nil) #destructive
+  difference = min_size - array.length
+  if difference <= 0
+    array
+  else
+    difference.times{ array.push(value) }
+    array
+  end
+end
+
+def pad(array, min_size, value = nil) #non-destructive
+  clone = array.clone
+  difference = min_size - clone.length
+  if difference <= 0
+    clone
+  else
+    difference.times{ clone.push(value) }
+    clone
+  end
+end
+
+# array = [1, 2, 3, 4]
+# p pad!(array, 3, "apples")
+
+# # 4. Reflection
+
+# Were you successful in breaking the problem down into small steps?
+# 	Yes! We knew exactly how we wanted to solve this and followed our psuedocode.
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+# 	Yes. We realized late that we needed to call .clone on the new array.
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+# 		We pased most tests, only failed pad being non-destructive. We fixed it with .clone.
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+# 	Not really, we felt that the solution was good enough, we clarified variable names.
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+# 	Very, yes.
+# What is the difference between destructive and non-destructive methods in your own words?
+# 	Destrutive alters the source. Non destructive leaves it in tact.
 
 
-# 4. Reflection
+
+
